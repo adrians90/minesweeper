ac3c426172419b20093569182a89f66635c4c546
Adrian <adriansandu9090@gmail.com>
2024-03-04 16:39:29 +0000

    app working as expected

--- minesweeper.js
+++ minesweeper.js
@@ -1,5 +1,5 @@
 // Logic
-const TILE_STATUSES = {
+export const TILE_STATUSES = {
     HIDDEN: "hidden",
     MINE: "mine",
     NUMBER: "number",
@@ -48,6 +48,25 @@ export function markTile(tile) {
     }
 }
 
+export function revealTile(board, tile) {
+    if (tile.status !== TILE_STATUSES.HIDDEN) {
+        return
+    }
+    if (tile.mine) {
+        tile.status = TILE_STATUSES.MINE
+        return 
+    }
+
+    tile.status = TILE_STATUSES.NUMBER
+    const adjacentTiles = nearbyTiles(board, tile)
+    const mines = adjacentTiles.filter(t => t.mine)
+    if (mines.length === 0) {
+        adjacentTiles.forEach(revealTile.bind(null,board))
+    } else {
+        tile.element.textContent = mines.length
+    }
+}
+
 function getMinePositions(boardSize, numberOfMines) {
     const positions = []
 
@@ -68,4 +87,33 @@ function positionMatch(a, b) {
 }
 function randomNumber(size) {
     return Math.floor(Math.random() * size)
+}
+
+function nearbyTiles(board, { x, y }) {
+    const tiles = []
+    for (let xOffset = -1; xOffset <= 1; xOffset++) {
+        for (let yOffset = -1; yOffset <= 1; yOffset++) {
+            const tile = board[x + xOffset]?.[y + yOffset]
+            if(tile) tiles.push(tile)
+        }
+    }
+    return tiles 
+
+}
+
+export function checkWin(board) {
+    return board.every(row => {
+        return row.every(tile => {
+            return tile.status === TILE_STATUSES.NUMBER || (tile.mine && (tile.status === TILE_STATUSES.HIDDEN || tile.status === TILE_STATUSES.MARKED))
+        })
+    })
+
+}
+
+export function checkLose(board) {
+    return board.some(row => {
+        return row.some(tile => {
+            return tile.status === TILE_STATUSES.MINE
+        })
+    })
 }
