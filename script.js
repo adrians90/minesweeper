1d4d61903ff9096f7c3eb92e10d1fabcf3c2a5d8
Adrian <adriansandu9090@gmail.com>
2024-03-04 15:00:43 +0000

    imported createBoard

---
+++ script.js
@@ -0,0 +1,13 @@
+//Display/UI
+import { createBoard } from "./minesweeper"
+
+
+
+//1. Populate the board with tiles/ mines
+//2. left click on tiles
+    //reveal tiles
+//3. right click on tiles
+    //mark tiles
+//4. check for win/lose
+
+
