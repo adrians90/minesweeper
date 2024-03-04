cbe35a74896cd6cc73d538d650d14b2ed3ef07b3
Adrian <adriansandu9090@gmail.com>
2024-03-04 15:12:59 +0000

    add get and set status to the tile object

--- minesweeper.js
+++ minesweeper.js
@@ -16,7 +16,13 @@ export function createBoard(boardSize, numberOfMines) {
             const tile = {
                 element,
                 x,
-                y
+                y,
+                get status() {
+                    return this.element.dataset.status
+                },
+                set status(value) {
+                    this.element.dataset.status = value
+                }
             }
 
             row.push(tile)
